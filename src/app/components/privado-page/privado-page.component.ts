55101cde954ff6c76d645fb8166c4ae1c4253d76
Martinez24 <ponchomarpez@gmail.com>
2018-09-20 16:36:42 -0500

    Creacion de componentes

---
+++ src/app/components/privado-page/privado-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privado-page',
+  template: `
+    <p>
+      privado-page works!
+    </p>
+  `,
+  styles: []
+})
+export class PrivadoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/register-page/register-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  template: `
+    <p>
+      register-page works!
+    </p>
+  `,
+  styles: []
+})
+export class RegisterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
